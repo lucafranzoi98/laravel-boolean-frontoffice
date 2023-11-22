214e24847e5208cc87f06fe6704c83ea99c709f6
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-11-22 17:44:33 +0100

    fix router.js

--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
-import { createRouter, createWebHashHistory} from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HomeView from './view/HomeView.vue';
-import AboutView from './view/AboutView.vue';
-import ContactsView from './view/ContactsView.vue';
+/* import AboutView from './view/AboutView.vue';
+import ContactsView from './view/ContactsView.vue'; */
 
 const router = createRouter({
-    history: createWebHashHistory();
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
             name: 'home',
             component: HomeView
         },
-        {
+        /* {
             path: '/about',
             name: 'about',
             component: AboutView
@@ -21,7 +21,7 @@ const router = createRouter({
             path: '/contacts',
             name: 'contacts',
             component: ContactsView
-        }
+        } */
     ]
 });
 
