b237bd11fecefd5cef52c6ad7a696bdea9a70c19
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-11-22 17:16:25 +0100

    Add card

---
+++ src/AppCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'AppCard',
+    props: {
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100 bg-dark">
+
+        <img :src="cocktail.thumb" alt="">
+
+        <div class="card-body h-100 d-flex flex-column justify-content-around">
+            <h3 class="text-center text-truncate text-danger">{{ cocktail.name }}</h3>
+            <div class="d-flex justify-content-center flex-wrap">
+                <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in cocktail.ingredients">{{
+                    ingredient.name }}</small>
+                <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in cocktail.measures">{{
+                    measure.name }}</small>
+            </div>
+            <p class="card-text text-white placeholder-glow">
+                {{ cocktail.instruction }}
+            </p>
+            <div class="col">
+
+            </div>
+            <div class="d-flex justify-content-between py-1">
+                <h4 class="text-light">{{ cocktail.alcholic }}</h4>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped> </style>
