05b66fee61373a2d7a025409044a9245f68c8497
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-11-22 17:13:53 +0100

    add home with jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script>
-export default {
+import HomeView from './view/HomeView.vue';
 
+export default {
+    components: { HomeView }
 }
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-6 m-auto">
-                <h1>ciao</h1>
-                <h2>a tutti</h2>
-                <h3>ma proprio tutti</h3>
-            </div>
-        </div>
-    </div>
+    <HomeView></HomeView>
 </template>
---
+++ src/view/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="p-5 mb-4 bg-light rounded-3 bg-dark w-75 m-auto shadow mt-2">
+        <div class="container  py-5 ">
+            <h1 class="display-5 fw-bold text-danger">CocktailBar for Dev</h1>
+            <p class="col-md-8 fs-4 text-white">After a day on the code, the best way to restore your mind</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeView'
+}
+</script>
+
+<style lang="scss" scoped></style>
