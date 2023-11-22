e73068a6a184b964aa331fbf5ada3bb4cd102bf3
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-11-22 16:23:10 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <template>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-6 m-auto">
+                <h1>ciao</h1>
+            </div>
+        </div>
+    </div>
 </template>
