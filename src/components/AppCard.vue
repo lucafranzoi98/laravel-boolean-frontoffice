84bf2b7de4415fd59db6d1407ae36577e4d1dacf
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-11-23 16:17:00 +0100

    add cocktail view api call

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -10,39 +10,42 @@ export default {
         }
     },
     methods: {
-      
+
     }
 }
 </script>
 
 <template>
-    <div class="card h-100 bg-dark">
+    <router-link :to="{ name: 'Cocktail', params: { slug: cocktail.slug } }">
 
-        <img :src="cocktail.thumb" alt="">
+        <div class="card h-100 bg-dark">
 
-        <div class="card-body h-100 d-flex flex-column justify-content-around">
-            <h3 class="text-center text-truncate text-danger">{{ cocktail.name }}</h3>
-            <div class="d-flex justify-content-center flex-wrap">
-                 <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in cocktail.ingredients">{{
-                    ingredient}}</small>
+            <img :src="cocktail.thumb" alt="">
+
+            <div class="card-body h-100 d-flex flex-column justify-content-around">
+                <h3 class="text-center text-truncate text-danger">{{ cocktail.name }}</h3>
+                <div class="d-flex justify-content-center flex-wrap">
+                    <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in cocktail.ingredients">{{
+                        ingredient }}</small>
                     <div class="text-white">
 
                         {{ cocktail.ingredients }}
                     </div>
-                <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in cocktail.measures">{{
-                    measure }}</small>
-            </div>
-            <p class="card-text text-white placeholder-glow">
-                {{ cocktail.instruction }}
-            </p>
-            <div class="col">
+                    <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in cocktail.measures">{{
+                        measure }}</small>
+                </div>
+                <p class="card-text text-white placeholder-glow">
+                    {{ cocktail.instruction }}
+                </p>
+                <div class="col">
 
-            </div>
-            <div class="d-flex justify-content-between py-1">
-                <h4 class="text-light">{{ cocktail.alcholic }}</h4>
+                </div>
+                <div class="d-flex justify-content-between py-1">
+                    <h4 class="text-light">{{ cocktail.alcholic }}</h4>
+                </div>
             </div>
         </div>
-    </div>
+    </router-link>
 </template>
 
 
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HomeView from './view/HomeView.vue';
 import AboutView from './view/AboutView.vue';
+import CocktailView from './view/CocktailView.vue';
+
 /* import ContactsView from './view/ContactsView.vue'; */
 
 const router = createRouter({
@@ -12,16 +14,21 @@ const router = createRouter({
             name: 'home',
             component: HomeView
         },
-         {
+        {
             path: '/about',
             name: 'about',
             component: AboutView
         },
-       /* {
-            path: '/contacts',
-            name: 'contacts',
-            component: ContactsView
-        } */
+        {
+            path: '/Cocktails/:slug',
+            name: 'Cocktail',
+            component: CocktailView
+        },
+        /* {
+             path: '/contacts',
+             name: 'contacts',
+             component: ContactsView
+         } */
     ]
 });
 
