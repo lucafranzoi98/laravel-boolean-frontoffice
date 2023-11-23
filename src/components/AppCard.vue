f471b7a4a6a6916aa499a2cb10ae5aaa306b1382
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-23 16:00:38 +0100

    pass all data to cards

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -6,13 +6,22 @@ export default {
     },
     data() {
         return {
-
+            ingredients: [],
+            measures: []
         }
     },
     methods: {
-        jsonDecode (){
-            JSON.parse(this.cocktail.ingredients);
+        ingredientsDecode (){
+            this.ingredients = JSON.parse(this.cocktail.ingredients);
+        },
+
+        measuresDecode (){
+            this.measures = JSON.parse(this.cocktail.measures);
         }
+    },
+    mounted(){
+        this.ingredientsDecode();
+        this.measuresDecode();
     }
 }
 </script>
@@ -25,23 +34,22 @@ export default {
         <div class="card-body h-100 d-flex flex-column justify-content-around">
             <h3 class="text-center text-truncate text-danger">{{ cocktail.name }}</h3>
             <div class="d-flex justify-content-center flex-wrap">
-                <!-- <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in cocktail.ingredients">{{
-                    ingredient.name }}</small> -->
-                    <div class="text-white">
-
-                        {{ cocktail.ingredients }}
-                    </div>
-                <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in cocktail.measures">{{
-                    measure.name }}</small>
+                <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in this.ingredients">{{
+                    ingredient }}</small>
+                <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in this.measures">{{
+                    measure }}</small>
             </div>
             <p class="card-text text-white placeholder-glow">
-                {{ cocktail.instruction }}
+                {{ cocktail.instructions }}
             </p>
             <div class="col">
 
             </div>
             <div class="d-flex justify-content-between py-1">
                 <h4 class="text-light">{{ cocktail.alcholic }}</h4>
+            </div>
+            <div class="d-flex justify-content-between py-1">
+                <h4 class="text-light">{{ cocktail.category }}</h4>
             </div>
         </div>
     </div>
