f1ef2c6e0f00a9be4f1708ef66a73d62e20d358d
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-23 16:23:49 +0100

    fix bug

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -13,15 +13,15 @@ export default {
     methods: {
 
 
-        ingredientsDecode (){
+        ingredientsDecode() {
             this.ingredients = JSON.parse(this.cocktail.ingredients);
         },
 
-        measuresDecode (){
+        measuresDecode() {
             this.measures = JSON.parse(this.cocktail.measures);
         }
     },
-    mounted(){
+    mounted() {
         this.ingredientsDecode();
         this.measuresDecode();
 
@@ -31,33 +31,34 @@ export default {
 </script>
 
 <template>
-    <router-link :to="{ name: 'Cocktail', params: { slug: cocktail.slug } }">
+    <router-link class="text-decoration-none" :to="{ name: 'Cocktail', params: { slug: cocktail.slug } }">
 
         <div class="card h-100 bg-dark">
 
             <img :src="cocktail.thumb" alt="">
 
-        <div class="card-body h-100 d-flex flex-column justify-content-around">
-            <h3 class="text-center text-truncate text-danger">{{ cocktail.name }}</h3>
-            <div class="d-flex justify-content-center flex-wrap">
+            <div class="card-body h-100 d-flex flex-column justify-content-around">
+                <h3 class="text-center text-truncate text-danger">{{ cocktail.name }}</h3>
+                <div class="d-flex justify-content-center flex-wrap">
 
-                <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in this.ingredients">{{
-                    ingredient }}</small>
-                <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in this.measures">{{
-                    measure }}</small>
-            </div>
-            <p class="card-text text-white placeholder-glow">
-                {{ cocktail.instructions }}
-            </p>
-            <div class="col">
+                    <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in this.ingredients">{{
+                        ingredient }}</small>
+                    <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in this.measures">{{
+                        measure }}</small>
+                </div>
+                <p class="card-text text-white placeholder-glow">
+                    {{ cocktail.instructions }}
+                </p>
+                <div class="col">
 
-            </div>
-            <div class="d-flex justify-content-between py-1">
-                <h4 class="text-light">{{ cocktail.alcholic }}</h4>
+                </div>
+                <div class="d-flex justify-content-between py-1">
+                    <h4 class="text-light">{{ cocktail.alcholic }}</h4>
 
-            </div>
-            <div class="d-flex justify-content-between py-1">
-                <h4 class="text-light">{{ cocktail.category }}</h4>
+                </div>
+                <div class="d-flex justify-content-between py-1">
+                    <h4 class="text-light">{{ cocktail.category }}</h4>
+                </div>
             </div>
         </div>
     </router-link>
