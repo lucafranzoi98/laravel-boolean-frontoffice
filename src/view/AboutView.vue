b77e21ac80a0d857d1da0b3c878d85666e932c9c
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-24 16:46:32 +0100

    add css to about and contacts view

--- src/view/AboutView.vue
+++ src/view/AboutView.vue
@@ -2,56 +2,22 @@
 
 export default {
     name: 'AboutView',
-
-    props: {
-
-    },
-
-    methods: {
-
-    }
-
 }
 
 </script>
 
 <template>
-    <div class="container">
+    <div class="container py-5">
 
-        <h1 class="text-center">About US</h1>
+        <h1 class="text-white mb-4">About us</h1>
 
-        <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="p-5 bg-light rounded-3">
             <div class="container-fluid py-5">
               <h1 class="display-5 fw-bold">Hi there 👋</h1>
-              <p class="col-md-8 fs-4">🍸 Hi, we are Carlo, Elia, Danilo, Luca and Alessandro and this is our Cocktails site! 🍸</p>
-              <button class="btn btn-primary btn-lg" type="button">Button</button>
+              <p class="fs-4">🍸 Hi, we are Carlo, Elia, Danilo, Luca and Alessandro and this is our Cocktails site! 🍸</p>
             </div>
           </div>
 
-          <div>
-            <div class="row">
-                <div class="col">
-                    <h1>Carlo</h1>
-                    <i class="bi bi-instagram"></i>
-                </div>
-                <div class="col">
-                    <h1>Luca</h1>
-                    <i class="bi bi-instagram"></i>
-                </div>
-                <div class="col">
-                    <h1>Danilo</h1>
-                    <i class="bi bi-instagram"></i>
-                </div>
-                <div class="col">
-                    <h1>Elia</h1>
-                    <i class="bi bi-instagram"></i>
-                </div>
-                <div class="col">
-                    <h1>Alessandro</h1>
-                    <i class="bi bi-instagram"></i>
-                </div>
-            </div>
-          </div>
 
     </div>
 </template>
