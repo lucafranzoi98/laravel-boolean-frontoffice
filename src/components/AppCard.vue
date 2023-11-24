800121ef57e1e69385a705b2f8aec4d4a894978f
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-11-24 16:46:42 +0100

    Add some style for about us

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -39,25 +39,31 @@ export default {
 
             <div class="card-body h-100 d-flex flex-column justify-content-around">
                 <h3 class="text-center text-truncate text-danger">{{ cocktail.name }}</h3>
-                <div class="d-flex justify-content-center flex-wrap">
+                <div class="d-flex justify-content-center flex-wrap gap-3">
 
-                    <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in this.ingredients">{{
-                        ingredient }}</small>
-                    <small class="badge rounded-pill text-bg-primary m-1" v-for="measure in this.measures">{{
-                        measure }}</small>
+                    <span class="badge rounded-pill text-bg-info bg-light text-center">
+                        <small class="badge rounded-pill text-bg-danger m-1" v-for="ingredient in this.ingredients">{{
+                            ingredient }}</small>
+                    </span>
+                    <span class="badge rounded-pill text-bg-info bg-light text-center">
+                        <small class="badge rounded-pill text-bg-primary  bg-dark m-1" v-for="measure in this.measures">{{
+                            measure }}</small>
+                    </span>
                 </div>
-                <p class="card-text text-white placeholder-glow">
-                    {{ cocktail.instructions }}
-                </p>
+
                 <div class="col">
 
                 </div>
-                <div class="d-flex justify-content-between py-1">
-                    <h4 class="text-light">{{ cocktail.alcholic }}</h4>
-
-                </div>
-                <div class="d-flex justify-content-between py-1">
-                    <h4 class="text-light">{{ cocktail.category }}</h4>
+                <div class="container d-flex justify-content-between py-2 align-items-center">
+                    <div class="d-flex justify-content-between py-2 ">
+                        
+                        <h4 class="text-danger" v-if="this.cocktail.alcholic === 'Alcoholic'">{{ cocktail.alcholic }}</h4>
+                        <h4 class="text-success" v-else>{{ cocktail.alcholic }}</h4>
+                    </div>
+                    <div class="d-flex justify-content-between py-1">
+                        <h4 class="text-warning" v-if="this.cocktail.category === 'Cocktail'">{{ cocktail.category }}</h4>
+                        <h4 class="text-primary" v-else>{{ cocktail.category }}</h4>
+                    </div>
                 </div>
             </div>
         </div>
@@ -66,28 +72,24 @@ export default {
 
 
 <style lang="scss" scoped>
-
 .card {
-  border-radius: 15px;
-  background: white;
-  border: 2px solid white;
-  display: flex;
-  transition: 0.4s ease-out;
-  box-shadow: 0px 7px 10px rgba(black, 0.5);
+    border-radius: 15px;
+    background: white;
+    border: 2px solid white;
+    display: flex;
+    transition: 0.4s ease-out;
+    box-shadow: 0px 7px 10px rgba(black, 0.5);
 
-  &:hover {
-    transform: translateY(20px);
-}
-    &:before {
-      opacity: 1;
+    &:hover {
+        transform: translateY(20px);
     }
-     
+
+
     img {
-    width: 100% ;
-    height: 100% ;
-    object-fit: cover ;
-    border-radius: 15px ;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 15px;
     }
 }
-
 </style>
--- src/view/AboutView.vue
+++ src/view/AboutView.vue
@@ -16,46 +16,110 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="container main_content ">
 
-        <h1 class="text-center">About US</h1>
+        <h1 class="text-center text-light pb-5">About US</h1>
 
-        <div class="p-5 mb-4 bg-light rounded-3">
-            <div class="container-fluid py-5">
-              <h1 class="display-5 fw-bold">Hi there 👋</h1>
-              <p class="col-md-8 fs-4">🍸 Hi, we are Carlo, Elia, Danilo, Luca and Alessandro and this is our Cocktails site! 🍸</p>
-              <button class="btn btn-primary btn-lg" type="button">Button</button>
+        <div class="p-5 mb-4 bg-dark text-light  rounded-3">
+            <div class="container-fluid py-5 mb-5 d-flex justify-content-around ">
+                <h1 class="display-5 fw-bold">Hi there 👋</h1>
+                <p class="col-md-8 fs-4">🍸 Hi, we are Carlo, Elia, Danilo, Luca and Alessandro and this is our Cocktails
+                    site! 🍸</p>
             </div>
-          </div>
+        </div>
 
-          <div>
-            <div class="row">
+        <div>
+            <div class="row py-5 mt-5">
                 <div class="col">
-                    <h1>Carlo</h1>
-                    <i class="bi bi-instagram"></i>
+                    <div class="container card-info text-light  bg-dark text-center">
+                        <h1>Carlo</h1>
+                        <div class="info ">
+                            <p>Nome: Carlo</p>
+                            <p>Cognome: Fanelli</p>
+                            <p>Professione: Full Stack Web Developer</p>
+                            <a href="">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    <h1>Luca</h1>
-                    <i class="bi bi-instagram"></i>
+                    <div class="container card-info text-light  bg-dark text-center">
+                        <h1>Luca</h1>
+                        <div class="info">
+                            <p>Nome: Luca</p>
+                            <p>Cognome: Franzoi</p>
+                            <p>Professione: Full Stack Web Developer</p>
+                            <a href="">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    <h1>Danilo</h1>
-                    <i class="bi bi-instagram"></i>
+                    <div class="container card-info text-light  bg-dark text-center">
+                        <h1>Danilo</h1>
+                        <div class="info">
+                            <p>Nome: Danilo</p>
+                            <p>Cognome: Salerno</p>
+                            <p>Professione: Full Stack Web Developer</p>
+                            <a href="">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    <h1>Elia</h1>
-                    <i class="bi bi-instagram"></i>
+                    <div class="container card-info text-light bg-dark text-center">
+                        <h1>Elia</h1>
+                        <div class="info">
+                            <p>Nome: Elia</p>
+                            <p>Cognome: Buratti</p>
+                            <p>Professione: Full Stack Web Developer</p>
+                            <a href="">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    <h1>Alessandro</h1>
-                    <i class="bi bi-instagram"></i>
+                    <div class="container card-info text-light  text-center bg-dark">
+                        <h1>Alessandro</h1>
+                        <div class="info">
+                            <p>Nome: Alessandro</p>
+                            <p>Cognome: Saladino</p>
+                            <p>Professione: Full Stack Web Developer</p>
+                            <a href="" class="">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
+        </div>
 
     </div>
 </template>
 
 <style lang="scss" scoped>
+.card-info {
+    border: 5px solid black;
+    border-radius: 20px;
 
+    & .info {
+        display: none;
+        text-align: start;
+    }
+
+    &:hover {
+        .info{
+            display: block;
+        }
+    }
+}
+
+.main_content {
+    height: 1100px;
+    padding-top: 10vh;
+}
 </style>
